809e8a534c6befa6c2ab3b568fabbdec02f48539
Luis Carrion <lcarrionalvarez@gmail.com>
2022-04-07 15:56:48 -0500

    Visor simple de pdf usando pdf.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const { PDFDocument, StandardFonts, rgb } = require("pdf-lib");
-const { writeFileSync, readFileSync } = require("fs");
-
-async function createPDF() {
-  const document = await PDFDocument.load(readFileSync("./jane-doe.pdf"));
-  //const me = document.findPageForAnnotationRef(12)
-  //console.log(me)
-}
-createPDF();
